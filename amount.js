635f93c35552b67d9a11524068e9b0cc31e887d9
DexZed <144045080+DexZed@users.noreply.github.com>
2024-09-24 23:30:10 +0600

    "Updated amount.js: added input value reset, modified history entry HTML structure and styling"

--- amount.js
+++ amount.js
@@ -11,6 +11,7 @@ function updateAmount(divId, inputId) {
 
     if (isNaN(inputValue) || inputValue <= 0) {
         alert('Please enter a valid amount.');
+        inputElement.value="";
         return;
     }
 
@@ -33,18 +34,18 @@ function updateAmount(divId, inputId) {
     targetDiv.querySelector('span').textContent = currentDivAmount;
 
    // New history entry
-   let content = targetDiv.querySelector('h3').textContent.trim(); // Get content from the target div
+   let content = targetDiv.querySelector('h3').textContent.trim(); 
     
     //New history entry HTML structure
     const historyEntry = document.createElement('div');
     historyEntry.className = "flex flex-col rounded-2xl w-full bg-[#ffffff] shadow-xl p-3 border-2";
     historyEntry.innerHTML = `
     <div class="flex flex-col p-8">
-        <div class="text-xl font-bold text-center text-[#374151] pb-6">
-            ${inputValue} donated for ${content}
+        <div class="text-xl font-bold text-center text-[#374151] pb-6 sm:text-start">
+            ${inputValue} taka donated for ${content}
         </div>
-        <div class="text-base text-center text-[#374151] font-light">
-            ${new Date().toLocaleString()}
+        <div class="text-base text-center text-[#374151] font-light sm:text-start">
+            Date: ${new Date().toString()}
         </div>
     </div>
     `;
@@ -52,4 +53,5 @@ function updateAmount(divId, inputId) {
     const historyContainer = document.getElementById('history-container');
     historyContainer.appendChild(historyEntry);
     document.getElementById('my_modal_5').showModal();
+    inputElement.value="";
 }
