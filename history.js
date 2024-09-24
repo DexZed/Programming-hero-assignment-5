861ff7b93e18da175a55ce17541903b2b43ef38e
DexZed <144045080+DexZed@users.noreply.github.com>
2024-09-24 21:09:39 +0600

    "Updated error.html, history.html, and index.html with changes to button elements, added new div elements, and modified existing div elements."

---
+++ history.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const donbtn = document.getElementById("donbtn");
+    const hisbtn = document.getElementById("hisbtn");
+    const don = document.getElementById("donations");
+    const his = document.getElementById("history");
+
+    // Function to show donations section and hide history section
+    function showDonations() {
+        don.classList.remove("hidden");
+        his.classList.add("hidden");
+        donbtn.classList.add("bg-[#B4F461]"); 
+        donbtn.classList.remove("bg-white"); 
+        hisbtn.classList.add("bg-white"); 
+        hisbtn.classList.remove("bg-[#B4F461]"); 
+    }
+
+    // Function to show history section and hide donations section
+    function showHistory() {
+        his.classList.remove("hidden");
+        don.classList.add("hidden");
+        hisbtn.classList.add("bg-[#B4F461]"); 
+        hisbtn.classList.remove("bg-white"); 
+        donbtn.classList.add("bg-white"); 
+        donbtn.classList.remove("bg-[#B4F461]"); 
+    }
+
+    donbtn.addEventListener("click", showDonations);
+    hisbtn.addEventListener("click", showHistory);
+});
